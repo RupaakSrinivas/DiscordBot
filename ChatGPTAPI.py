60c30bfbc90b7197ac6cebcf594c27a440d802d9
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2023-04-11 15:22:14 +0530

    Create ChatGPTAPI.py
    This is a standalone ChatGPT API, this is a mvp and a test to ensure that API works.

---
+++ ChatGPTAPI.py
@@ -0,0 +1,24 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
+openai.api_key = api_key
+messages = [ {"role": "system", "content": "You are a intelligent assistent."} ]
+
+
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role":"user","content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append( {"role": "assistant", "content": reply} )
